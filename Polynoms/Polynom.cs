0bb4152307e8b4e335fa25a317f125f50b5181b1
Infarh <shmachilin@yandex.ru>
2022-02-08 10:44:36 +0300

    Функция расчёта значения полинома по массиву его коэффициентов

--- Polynoms/Polynom.cs
+++ Polynoms/Polynom.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace Polynoms
 {
-    internal class Polynom
+    class Polynom
     {
+        public static double GetValue(double[] a, double x)
+        {
+            double sum = 0;
 
+            for (var i = 0; i < a.Length; i++)
+            {
+                var value = a[i] * Math.Pow(x, i);
+
+                sum = sum + value;
+            }
+
+            return sum;
+        }
     }
 }
