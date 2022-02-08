d7a8269ce00d52e3ef95efdc7e260ceecb315c60
Infarh <shmachilin@yandex.ru>
2022-02-08 10:46:50 +0300

    Тест полинома

--- Polynoms/Program.cs
+++ Polynoms/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using Polynoms;
+
+double[] P = { -3, -7, 2, 5 };
+
+double x = 5;
+
+var y = Polynom.GetValue(P, x);
+
+Console.WriteLine("y(x) = {0}", y);
