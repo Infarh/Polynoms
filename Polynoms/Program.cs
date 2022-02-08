34bf286b88d76bbce06dbc5aeb8094ace60eee17
Infarh <shmachilin@yandex.ru>
2022-02-08 10:25:00 +0300

    Новая структура файла

--- Polynoms/Program.cs
+++ Polynoms/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace Polynoms
-{
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿Console.WriteLine("Hello, World!");
