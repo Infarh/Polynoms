124079123f0360cac562dd22754504923b50f97a
Infarh <shmachilin@yandex.ru>
2022-02-15 09:22:36 +0300

    Добавлен проект модульного тестирования

---
+++ Tests/Polynoms.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Polynoms.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var x = 5;
+            var y = 0;
+
+            var z = x / y;
+        }
+    }
+}
