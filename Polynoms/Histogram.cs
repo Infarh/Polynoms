04ce98c0e73079a3a495bee0526250959e0287a8
Infarh <shmachilin@yandex.ru>
2022-02-22 11:14:10 +0300

    Реализация гистограммы

---
+++ Polynoms/Histogram.cs
@@ -0,0 +1,84 @@
+﻿namespace Polynoms;
+
+public class Histogram
+{
+    private int[] _Counts;
+
+    public int IntervalsCount { get; }
+    public int SamplesCount { get; }
+
+    public double Min { get; }
+    public double Max { get; }
+
+    public double IntervalLength => Max - Min;
+
+    public double dx { get; }
+
+    public double Mean { get; }
+    public double Variance { get; }
+    public double StdDev { get; }
+
+    public Histogram(double[] Samples, int IntervalsCount)
+    {
+        this.IntervalsCount = IntervalsCount;
+        SamplesCount = Samples.Length;
+
+        var min = double.PositiveInfinity;
+        var max = double.NegativeInfinity;
+
+        var sum = 0d;
+        var sum2 = 0d;
+
+        foreach (var x in Samples)
+        {
+            if (x < min)
+                min = x;
+
+            if (x > max)
+                max = x;
+
+            sum += x;
+            sum2 += x * x;
+        }
+
+        var average = sum / Samples.Length;                         // мат.ожидание (среднее по выборке)
+        var variance = sum2 / Samples.Length - average * average;   // дисперсия
+        var stddev = Math.Sqrt(variance);                           // СКО
+
+        Min = min;
+        Max = max;
+        Mean = average;
+        Variance = variance;
+        StdDev = stddev;
+
+        var dx = IntervalLength / IntervalsCount;
+        this.dx = dx;
+
+        var counts = new int[IntervalsCount];
+        _Counts = counts;
+        foreach (var x in Samples)
+        {
+            for (var i = 0; i < IntervalsCount; i++)
+            {
+                var interval_min = min + i * dx;
+                var interval_max = interval_min + dx;
+
+                if (i < IntervalsCount - 1)
+                {
+                    if (x >= interval_min && x < interval_max)
+                    {
+                        counts[i]++;
+                    }
+                }
+                else
+                {
+                    if (x >= interval_min && x <= interval_max)
+                    {
+                        counts[i]++;
+                    }
+                }
+            }
+        }
+
+    }
+}
--- Polynoms/Program.cs
+++ Polynoms/Program.cs
@@ -1,3 +1,27 @@
 ﻿
+using Polynoms;
+
+var rnd = new Random();
+
+const int count = 100_000;
+
+var samples = new double[count];
+
+for (var i = 0; i < count; i++)
+{
+    var x = (rnd.NextDouble() * 2 - 1)
+        + (rnd.NextDouble() * 2 - 1)
+        + (rnd.NextDouble() * 2 - 1)
+        + (rnd.NextDouble() * 2 - 1)
+        + (rnd.NextDouble() * 2 - 1)
+        ;
+
+    samples[i] = x / 5;
+}
+
+
+const int intervals_count = 20;
+var histogram = new Histogram(samples, intervals_count);
+
 
 Console.WriteLine();
