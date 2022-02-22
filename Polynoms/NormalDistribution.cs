5e5b6f3444c0b715ed3d2898831efedae35e6e71
Infarh <shmachilin@yandex.ru>
2022-02-22 11:30:44 +0300

    Построение гистограммы

---
+++ Polynoms/NormalDistribution.cs
@@ -0,0 +1,10 @@
+﻿namespace Polynoms;
+
+public static class NormalDistribution
+{
+    public static double Distribution(double x, double mu = 0, double sigma = 1)
+    {
+        var xx = (x - mu) / sigma;
+        return Math.Exp(-0.5 * xx * xx) / (sigma * Math.Sqrt(Math.PI * 2));
+    }
+}
--- Polynoms/Program.cs
+++ Polynoms/Program.cs
@@ -1,4 +1,8 @@
 ﻿
+using OxyPlot;
+using OxyPlot.ImageSharp;
+using OxyPlot.Series;
+
 using Polynoms;
 
 var rnd = new Random();
@@ -23,5 +27,38 @@ for (var i = 0; i < count; i++)
 const int intervals_count = 20;
 var histogram = new Histogram(samples, intervals_count);
 
+var histogram_values = new HistogramItem[intervals_count];
+for (var i = 0; i < intervals_count; i++)
+{
+    var min = histogram.Min + i * histogram.dx;
+    var max = min + histogram.dx;
+    histogram_values[i] = new HistogramItem(min, max, histogram[i], 0);
+}
 
-Console.WriteLine();
+var plot = new PlotModel
+{
+    Title = "Гистограмма выборки случайной величины",
+    Background = OxyColors.White,
+    Series =
+    {
+        new HistogramSeries
+        {
+            FillColor = OxyColors.Blue,
+            StrokeColor = OxyColors.DarkBlue,
+            StrokeThickness = 1,
+            ItemsSource = histogram_values
+        },
+        new FunctionSeries(x => NormalDistribution.Distribution(x), histogram.Min, histogram.Max, histogram.dx / 20)
+        {
+            Color = OxyColors.Red,
+        }
+    }
+};
+
+
+var exporter = new PngExporter(800, 600);
+
+using(var histogram_file = File.Create("histogram.png"))
+    exporter.Export(plot, histogram_file);
+
+//Console.WriteLine();
