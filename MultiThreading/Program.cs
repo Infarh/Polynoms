fdb6271847444a385ea793e74c4b085b0fca2de4
Infarh <shmachilin@yandex.ru>
2022-03-29 09:55:02 +0300

    Свойства потока

--- MultiThreading/Program.cs
+++ MultiThreading/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using System.Diagnostics;
 
 namespace MultiThreading
 {
@@ -6,8 +7,28 @@ namespace MultiThreading
     {
         static void Main(string[] args)
         {
+            //Process cmd_process = Process.Start("cmd.exe");
+            //Process calc_process = Process.Start("cmd.exe");
+            Process current = Process.GetCurrentProcess();
+            current.PriorityClass = ProcessPriorityClass.AboveNormal;
+            //current.Kill();
+            //current.CloseMainWindow();
+            Console.WriteLine("Current PID:{0}", current.Id);
+
+            // Способы организации многопоточности
+            // 1. Корпоративная многопоточность
+            // 2. Вытесняющая многопоточность
+
             var timer_thread = new Thread(UpdateHeaderTime);
+            timer_thread.IsBackground = true; // сделать поток фоновым
+            timer_thread.Name = "Поток часов";
+            timer_thread.Priority = ThreadPriority.AboveNormal;
+            //timer_thread.IsAlive
+            
+
             timer_thread.Start();
+            var timer_tid = timer_thread.ManagedThreadId;
+            Console.WriteLine("Timer thread id:{0}", timer_tid);
 
             Console.WriteLine("1231231213");
             Console.ReadLine();
@@ -17,7 +38,8 @@ namespace MultiThreading
         {
             while (true)
             {
-                Console.Title = DateTime.Now.ToString("HH:mm:ss.ffff");
+                Console.Title = DateTime.Now.ToString("HH:mm:ss.fff");
+                Thread.Sleep(100);
             }
         }
     }
