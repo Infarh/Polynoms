c52fc58462648553707b3883bc86b081c646bbe8
Infarh <shmachilin@yandex.ru>
2022-03-29 09:26:02 +0300

    Пример создания и запуска потока

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+
+namespace MultiThreading
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            var timer_thread = new Thread(UpdateHeaderTime);
+            timer_thread.Start();
+
+            Console.WriteLine("1231231213");
+            Console.ReadLine();
+        }
+
+        private static void UpdateHeaderTime()
+        {
+            while (true)
+            {
+                Console.Title = DateTime.Now.ToString("HH:mm:ss.ffff");
+            }
+        }
+    }
+}
